1c89b3f1f103d4164cee81b1582b1aa0c922fa1c
prateek173 <114941346+prateek173@users.noreply.github.com>
2023-10-23 18:14:57 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Email: prateek.anand2021@vitstudent.ac.in</p>
+      <p>Phone: 7388581181</p>
+      <p>LinkedIn:https://www.linkedin.com/in/prateek-anand-06013b216?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app</p>
+    </div>
+  );
+};
+
+export default Contact;
